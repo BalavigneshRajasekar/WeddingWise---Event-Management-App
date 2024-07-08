ee875db1fd75c32eb3a4fbfa85228545412cd055
BalavigneshRajasekar <you@example.com>
2024-07-08 11:55:53 +0530

    API Added

--- Frontend/src/Routes/Malls.jsx
+++ Frontend/src/Routes/Malls.jsx
@@ -164,7 +164,7 @@ function Malls() {
     });
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/malls/add",
+        "https://eventapi-uk2d.onrender.com/api/malls/add",
         formData,
         {
           headers: {
